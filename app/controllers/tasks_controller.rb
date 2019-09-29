9f86823a1289c811734d65c243f72b926d5c5951
Héctor Espinoza <hespinoza@uchile.cl>
2019-09-29 17:05:12 -0300

    ToDos change its boolean status with button

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,6 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+    @todos = Todo.all
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'todos', to: 'todos#index', as: 'todos'
+  get 'todo/:todo_id', to: 'todos#show', as: 'todo'
+  patch 'todos/:todo_id', to: 'todos#update', as: 'update_todo'
+  devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'tasks#index'
+end
